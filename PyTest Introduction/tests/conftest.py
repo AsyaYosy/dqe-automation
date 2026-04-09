488b28a5469e0ab8660f8080731d547d90c5e2a0
AsyaYosy <anastasiayosypcuk07@gmail.com>
2026-04-09 21:54:29 +0200

    HW: Pytest Introduction

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -1,10 +1,31 @@
 import pytest
 import pandas as pd
 
+# Fixture to provide the path to the CSV file
+@pytest.fixture(scope="session")
+def path_to_file(pytestconfig):
+    return pytestconfig.rootpath.parent / "src" / "data" / "data.csv"
+
+
 # Fixture to read the CSV file
+@pytest.fixture(scope="session")
+def read_csv_file(path_to_file):
+    return pd.read_csv(path_to_file)
 
 
 # Fixture to validate the schema of the file
+@pytest.fixture(scope="session")
+def expected_schema():
+    return ["id", "name", "age", "email", "is_active"]
+
+@pytest.fixture(scope="session")
+def actual_schema(read_csv_file):
+    return list(read_csv_file.columns)
 
 
 # Pytest hook to mark unmarked tests with a custom mark
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        if not list(item.iter_markers()):
+            item.add_marker(pytest.mark.unmarked)
+
--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -1,30 +1,51 @@
 import pytest
 import re
 
-
-def test_file_not_empty():
-    assert 1 + 1 == 2
-
-
-def test_duplicates():
-    assert 1 + 1 == 2
+def test_file_not_empty(read_csv_file):
+    assert read_csv_file.empty, "CSV file is empty"
 
 
-def test_validate_schema():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+@pytest.mark.xfail
+def test_duplicates(read_csv_file):
+    duplicates = read_csv_file.duplicated()
+    assert not duplicates.any(), "CSV file contains duplicate rows"
 
 
-def test_age_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_validate_schema(actual_schema, expected_schema):
+    assert actual_schema == expected_schema, "Schema does not match expected schema"
+    
+
+@pytest.mark.validate_csv
+@pytest.mark.skip
+def test_age_column_valid(read_csv_file):
+    valid_age = [0, 100]
+    age = read_csv_file['age']
+    age_not_null = age[age.notna()]
+    assert age_not_null.between(valid_age[0], valid_age[1]).all(), "Age column contains invalid values"
 
 
-def test_email_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_email_column_valid(read_csv_file):
+    email = read_csv_file["email"]
+    email_not_null = email[email.notna()]
+    email_pattern = r"^[a-z0-9_.]+@[a-z0-9_.]+\.[a-z]{2,}$"
+    assert email_not_null.str.match(email_pattern).all(), "Email column contains invalid email addresses"
 
 
-def test_active_players():
-    assert 1 + 1 == 2
+def test_active_players(read_csv_file):
+    df = read_csv_file[["id", "is_active"]]
+    players_1 =  df[df["id"] == 1]["is_active"]
+    players_2 =  df[df["id"] == 2]["is_active"]
+    
+    assert (players_1 == False).all(), "Player with id 1 should be inactive"
+    assert (players_2 == True).all(), "Player with id 2 should be active"
 
 
-def test_active_player():
-    assert 1 + 1 == 2
+@pytest.mark.parametrize("id, expected_status", [(1, False), (2, True)])
+def test_active_player(read_csv_file, id, expected_status):
+    df = read_csv_file[["id", "is_active"]]
+    actual_status = df[df["id"] == id]["is_active"]
+
+    assert (actual_status == expected_status).all(), f"Player with id {id} should have is_active status {expected_status}"
